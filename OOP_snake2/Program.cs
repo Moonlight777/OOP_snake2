2a2b94bde7490b816150d446ead3b12c4ce8cf7f
Moonlight777 <moonliht777@gmail.com>
2020-05-06 12:14:40 -0700

    Encapsulation

--- OOP_snake2/Program.cs
+++ OOP_snake2/Program.cs
@@ -8,19 +8,30 @@ namespace OOP_snake2
         static void Main(string[] args)
         {
 
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
+            Point p1 = new Point(1, 3, '*'); // constructor. That called encapsulation. In object-oriented programming (OOP), encapsulation refers to the bundling of data with the methods that operate on that data, or the restricting of direct access to some of an object's components.[1] Encapsulation is used to hide the values or state of a structured data object inside a class, preventing unauthorized parties' direct access to them. Publicly accessible methods are generally provided in the class (so-called "getters" and "setters") to access the values, and other client classes call these methods to retrieve and modify the values within the object.
             p1.Draw();
 
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '%';
+            Point p2 = new Point(4, 5, '%');
             p2.Draw();
 
             Console.ReadLine();
+
+
+
+
+
+
+
+            /*  p1.x = 1;
+          p1.y = 3;
+          p1.sym = '*'; */
+
+
+
+            /*   p2.x = 4;
+            p2.y = 5;
+            p2.sym = '%'; */
+
 
             //    Draw(p1.x, p1.y, p1.sym);
             //    Draw(p2.x, p2.y, p2.sym);
