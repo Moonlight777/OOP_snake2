ab8f3c5ef9112ecec6a21697ccf9af0a7ff8ec16
Moonlight777 <moonliht777@gmail.com>
2020-05-06 11:59:06 -0700

    New Class and Method

---
+++ OOP_snake2/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_snake2
+{
+    class Point // class
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw() // method
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+
+    }
+}
--- OOP_snake2/Program.cs
+++ OOP_snake2/Program.cs
@@ -7,36 +7,54 @@ namespace OOP_snake2
     {
         static void Main(string[] args)
         {
-            //    Console.WriteLine("Hello World!");
-            //    Console.WriteLine();
 
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
 
-            Draw(x1, y1, sym1);
-
-           // Console.SetCursorPosition(x1, y1);
-           // Console.Write(sym1);
-
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '%';
-
-            // Console.SetCursorPosition(x2, y2);
-            // Console.Write(sym2);
-
-            Draw(x2, y2, sym2);
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '%';
+            p2.Draw();
 
             Console.ReadLine();
 
-        }
+            //    Draw(p1.x, p1.y, p1.sym);
+            //    Draw(p2.x, p2.y, p2.sym);
+            //    Console.WriteLine("Hello World!");
+            //    Console.WriteLine();
 
-        static void Draw(int x, int y, char sym)
+            /*  int x1 = 1;
+              int y1 = 3;
+              char sym1 = '*';
 
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
+
+              Draw(x1, y1, sym1); */
+
+            // Console.SetCursorPosition(x1, y1);
+            // Console.Write(sym1);
+
+            /*   int x2 = 4;
+               int y2 = 5;
+               char sym2 = '%';
+
+               // Console.SetCursorPosition(x2, y2);
+               // Console.Write(sym2);
+
+               Draw(x2, y2, sym2); */
+
+            /*   static void Draw(int x, int y, char sym)
+
+       {
+           Console.SetCursorPosition(x, y);
+           Console.Write(sym);
+
+       }
+
+      */
 
         }
     }
