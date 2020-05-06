acb969f969541f7bfb879487af04dbda0ad2fc92
Moonlight777 <moonliht777@gmail.com>
2020-05-06 11:45:59 -0700

    Function Draw added

--- OOP_snake2/Program.cs
+++ OOP_snake2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.SymbolStore;
 
 namespace OOP_snake2
 {
@@ -6,8 +7,37 @@ namespace OOP_snake2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine();
+            //    Console.WriteLine("Hello World!");
+            //    Console.WriteLine();
+
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+
+            Draw(x1, y1, sym1);
+
+           // Console.SetCursorPosition(x1, y1);
+           // Console.Write(sym1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '%';
+
+            // Console.SetCursorPosition(x2, y2);
+            // Console.Write(sym2);
+
+            Draw(x2, y2, sym2);
+
+            Console.ReadLine();
+
+        }
+
+        static void Draw(int x, int y, char sym)
+
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+
         }
     }
 }
